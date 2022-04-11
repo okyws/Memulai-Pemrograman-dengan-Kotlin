36403d1b27ea9d85065c612d7598d5062520292d
okyws <okywahyu1@gmail.com>
2022-04-11 15:51:15 +0700

    Submission Kelas Kotlin

---
+++ Functional/Function Inside Function/Inner function/src/Task.kt
@@ -0,0 +1,19 @@
+fun sum(valueA: Int, valueB: Int, valueC: Int): Int {
+    fun Int.validateNumber(){
+        if (this == 0) {
+            throw IllegalArgumentException("value must be better than 0")
+        }
+    }
+
+    valueA.validateNumber()
+    valueB.validateNumber()
+    valueC.validateNumber()
+
+    return valueA + valueB + valueC
+}
+
+fun main () {
+
+    print(sum(1,2,3))
+
+}
